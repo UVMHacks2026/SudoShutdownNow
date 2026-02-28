e0b0ec1ceac8a4414608d1b4204114c3796d0770
wenaf <wnorthfletcher@gmail.com>
2026-02-28 12:06:57 -0500

    updated framework

--- sveltTo.py
+++ sveltTo.py
@@ -1,15 +1,14 @@
-import base64
-
 from fastapi import FastAPI, File, UploadFile, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from PIL import Image
 import io
 
 # Need to update to active facial recognition code, this is test for now
-import facialRecTest  
+import facialRecTest
 
 app = FastAPI()
 
+# ---
 origins = [
     "http://cgswswk88cg04k4www8g8cgs.76.13.29.239.sslip.io/",
 ]
@@ -22,28 +21,20 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.post("/upload-image/")
-async def upload_image(file: UploadFile = File(...)):
-    if not file.content_type.startswith("image/"):
-        raise HTTPException(status_code=400, detail="Invalid file type. Please upload an image.")
-    try:
-        contents = await file.read()
-        image = Image.open(io.BytesIO(contents))
-        
-        # Convert image to JPEG format and encode as base64
-        jpeg_buffer = io.BytesIO()
-        image.convert("RGB").save(jpeg_buffer, format="JPEG")
-        jpeg_buffer.seek(0)
-        image_base64 = base64.b64encode(jpeg_buffer.getvalue()).decode("utf-8")
+app.websocket("/ws/stream")
+async def websocket_endpoint(websocket):
+    await websocket.accept()
+    print("WebSocket connection accepted")
     
-        # Call your facial recognition function here with the image
-        
-        
+    register_next_face = False
 
-        return {
-            "filename": file.filename,
-            "content_type": file.content_type,
-            "image_base64": image_base64
-        }
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Error processing image: {str(e)}")
+    try:
+        while True:
+            data = await websocket.receive_text()
+            print("Received string from WebSocket")
+            
+            # Parse the incoming data as an image
+            if "," in data:
+                base_64_data = data.split(",")[1]
+            else:
+                base_64_data = data

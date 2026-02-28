955b9dbee02337ad9622f6b72e81c5a226957e58
JonasH <johannes.hemmett@uvm.edu>
2026-02-28 14:59:01 -0500

    Uncomment

--- readEmployees.py
+++ readEmployees.py
@@ -192,12 +192,12 @@ def loadEmployees(reader, splitName=False):
     return employees
 
 if __name__ == "__main__":
-    # employees = formatReadEmployeeData("EmployeeDataTest.csv")
+    employees = formatReadEmployeeData("EmployeeDataTest.csv")
 
-    # if employees:
-    #     for employee in employees:
-    #         print(employees[employee])
-    #         print(employees[employee].getShifts())
+    if employees:
+        for employee in employees:
+            print(employees[employee])
+            print(employees[employee].getShifts())
         
     employees = readEmployeeData("EmployeeData.csv")
     if employees:

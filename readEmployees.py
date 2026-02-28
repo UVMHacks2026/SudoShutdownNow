542b93f5a235891865bd875101c0c48e1131cf8e
JonasH <johannes.hemmett@uvm.edu>
2026-02-28 10:24:01 -0500

    reading

--- readEmployees.py
+++ readEmployees.py
@@ -4,7 +4,19 @@ import Employee
 
 employees = {}
 
-with open("EmployeeData.csv", newline="") as csvFile:
-    reader = csv.DictReader(csvFile)
-    for row in reader:
-        employees[row["id"]] = Employee.Employee(row["firstName"], row["lastName"], row["id"])
+def readEmployees(fileName):
+    try:
+        with open(fileName, newline="") as csvFile:
+            reader = csv.DictReader(csvFile)
+            for row in reader:
+                if row["id"] in employees:
+                    print(f"Duplicate ID: {row["id"]}")
+                else:
+                    employees[row["id"]] = Employee.Employee(row["firstName"], row["lastName"], row["id"])
+    except FileNotFoundError:
+        print(f"Could not load the file: {fileName}")
+    except Exception as e:
+        print(f"An unexpected error has occured!: {e}")
+
+if __name__ == "__main__":
+    readEmployees("EmployeeData.csv")

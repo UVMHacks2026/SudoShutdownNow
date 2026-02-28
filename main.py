2d7c686bdd2e8009d27450738ca4e5736f143b64
Sorin Kacmarczyk <sorinja50@gmail.com>
2026-02-28 10:15:49 -0500

    adding some changes

---
+++ main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+import numpy as np # for storing face encodings
+
+
+app = FastAPI()
+
+@app.get("/")
+
+def root():
+    return {"message": "Hello World"}
+
+def get_data():
+    #TODO: Implement data retrieval logic here
+    # Get the employee
+    return {"data": "This is some data"}
+
+def process_data(data):
+    pass
